43eede411ad7b3f9c44765903cb415fe59af9bf0
Harris Michael <harrismic>
2008-09-19 15:56:58 +0000

    New domain classes
    SVN-Revision: 4925

--- grails-app/domain/gov/nih/nci/cma/domain/ClinicalColsNewDev.groovy
+++ grails-app/domain/gov/nih/nci/cma/domain/ClinicalColsNewDev.groovy
@@ -1,10 +1,24 @@
 package gov.nih.nci.cma.domain
 
 class ClinicalColsNewDev {
-
 	
+	 String colName
+	 String colType
+	 Integer accessCode
+	 Integer colNameId
+	 String displayName
+	 String dataSource
+	 String tableName
 	
-	
-	
-	
+     /*
+	 COL_NAME      VARCHAR2(100 BYTE),
+	  COL_TYPE      VARCHAR2(15 BYTE),
+	  ACCESS_CODE   NUMBER,
+	  COL_NAME_ID   NUMBER                          NOT NULL,
+	  DISPLAY_NAME  VARCHAR2(100 CHAR),
+	  DATA_SOURCE   VARCHAR2(30 BYTE),
+	  TABLE_NAME    VARCHAR2(40 BYTE),
+	  ID            NUMBER(19),
+	  VERSION       NUMBER(19)
+	  */
 }
