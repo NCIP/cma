46e38685b7142d0b7f41ad2be6622ba3b71d9296
Harris Michael <harrismic>
2008-09-25 20:12:55 +0000

    Added test call to analysis server .
    SVN-Revision: 4936

--- grails-app/controllers/GeneViewController.groovy
+++ grails-app/controllers/GeneViewController.groovy
@@ -1,6 +1,36 @@
+import gov.nih.nci.caintegrator.application.analysis.AnalysisHelper
+import gov.nih.nci.caintegrator.analysis.messaging.ReporterGroup
+import gov.nih.nci.caintegrator.analysis.messaging.SampleGroup
+import gov.nih.nci.caintegrator.service.findings.ExpressionLookupFinding
+import gov.nih.nci.caintegrator.analysis.messaging.DataPointVector
+
 class GeneViewController {
 
     def index = { 
+    	
+    	
+    	ReporterGroup rg = new ReporterGroup()
+    	rg.add("207848_at")
+    	    	    	
+    	
+    	SampleGroup sg = new SampleGroup()
+    	sg.add("TCGA-02-0071-01A-01R-0202-01")
+    	sg.add("TCGA-12-0618-01A-01R-0306-01")
+    	
+    	String rbinaryFileName  = "TCGA.affyhg-u133a_4_3_08.Rda"
+    	    	    
+    	//try sending a request
+    	ExpressionLookupFinding finding = AnalysisHelper.getExpressionValuesForReporters(rg, rbinaryFileName, sg)
+    	    	
+    	List<DataPointVector> dataVectors = finding.getDataVectors()
+    	
+    	if (dataVectors != null) {
+    	  System.out.println("Call to getExprValues returned numResults=${dataVectors.size()}")
+    	}
+    	else {
+    	  System.out.println("No data vectors returned")	
+    	}
+    	
     	render(view:'main')
     }
 }
