d18445d01d4259a5974c9a7e7872eccad660e49b
Ryan Landy <landyr>
2008-09-26 17:59:14 +0000

    removed
    SVN-Revision: 4963

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,4 +1,10 @@
 // Place your Spring DSL code here
 beans = {
-    
+		annotationManager(gov.nih.nci.caintegrator.domain.annotation.service.AnnotationManagerImpl) {
+			sessionFactory = ref("sessionFactory")
+		}
+		idMappingManager(gov.nih.nci.caintegrator.util.idmapping.IdMappingManager) {
+			sessionFactory = ref("sessionFactory")
+		}
+		
 }
