aac860936350fbf615c8995e32448e44626f65c9
Ryan Landy <landyr>
2008-09-26 02:23:18 +0000

    fixed lists, moved to service
    SVN-Revision: 4946

--- grails-app/controllers/gov/nih/nci/cma/controller/ApplicationUserController.groovy
+++ grails-app/controllers/gov/nih/nci/cma/controller/ApplicationUserController.groovy
@@ -4,16 +4,6 @@ import gov.nih.nci.security.SecurityServiceProvider;
 import gov.nih.nci.security.AuthenticationManager;
 import gov.nih.nci.security.exceptions.CSException;
 import gov.nih.nci.cma.domain.ApplicationUser;
-import gov.nih.nci.caintegrator.application.lists.UserListBean;
-import gov.nih.nci.caintegrator.application.lists.UserList;
-import gov.nih.nci.caintegrator.application.lists.ListItem;
-import gov.nih.nci.cma.CacheConstants;
-import gov.nih.nci.caintegrator.application.lists.UserListBeanHelper;
-import gov.nih.nci.caintegrator.application.lists.ListType;
-import gov.nih.nci.caintegrator.application.lists.ListOrigin;
-import gov.nih.nci.caintegrator.application.lists.ListSubType;
-import gov.nih.nci.caintegrator.application.cache.PresentationCacheManager;
-import gov.nih.nci.caintegrator.application.cache.PresentationTierCache;
 
 
 class ApplicationUserController extends BaseController {
@@ -21,7 +11,9 @@ class ApplicationUserController extends BaseController {
     def beforeInterceptor = [action:this.&auth, except:['login', 'logout']]
     
     def index = { redirect(action:list,params:params) }
-
+    
+    def defaultListLoaderService
+    
     // the delete, save and update actions only accept POST requests
     def allowedMethods = [delete:'POST', save:'POST', update:'POST']
 
@@ -115,8 +107,7 @@ class ApplicationUserController extends BaseController {
     		
     		
     		//check the local DB for the login
-    		def user =
-    			ApplicationUser.findByUserIdAndPassword(params.userId,
+    		def user = ApplicationUser.findByUserIdAndPassword(params.userId,
     					params.password)
     					
     		boolean loginOK  = (user != null)
@@ -154,79 +145,9 @@ class ApplicationUserController extends BaseController {
                  */
                  flash['message'] = 'Login Successful!'
                  
-                 PresentationTierCache presentationCacheManager = PresentationCacheManager.getInstance();
-                 //Login is successful now load the lists
-                 // Load user lists if necessary (patient lists and their permissable values)
-                 //UserListBean userListBean = (UserListBean) presentationCacheManager.getNonPersistableObjectFromSessionCache(request.getSession().getId(), CacheConstants.USER_LISTS);
-                 UserListBean userListBean = (UserListBean) session.getAttribute(CacheConstants.USER_LISTS);
-                 if (userListBean == null) {
-            	   userListBean = new UserListBean();
-            	   //session.setAttribute(CacheConstants.USER_LISTS, userListBean);
-            	   //presentationCacheManager.addNonPersistableToSessionCache(request.getSession().getId(), CacheConstants.USER_LISTS, userListBean);
-            	   
-            	   // load user lists 
-            	   // we are loading using grails and then converting to 
-            	   // list objects in application-commons so that we can reuse the list management 
-            	   // functionality w/o rewriting it in grail (for now).                   
-                   // List<UserList> userLists = (List<UserList>) listLoader.loadUserLists();
-                   def listObjs = gov.nih.nci.cma.domain.List.list()
+                 //use the injected service to load the def lists
+                 defaultListLoaderService.loadDefaultLists()
                    
-                   java.util.List<UserList> userLists = new ArrayList<UserList>();
-                   UserList ul;
-                   List<ListItem> items;
-                   
-                   listObjs.each{ gl ->
-                   
-                      def gItems = gl.listItems
-                      items = new ArrayList<ListItem>();
-                      gItems.each{ gi ->      
-                      ListItem i = new ListItem(gi.itemName, gi.listName)
-                    	  i.setId(gi.id);
-                    	  i.setRank(gi.rank)
-                    	  i.setNotes(gi.itemDescription)                    	                      	 
-                    	  items.add(i)
-                      }
-                                            
-                      ListType lt = ListType.valueOf(gl.type)
-                	  ul = new UserList(gl.name, lt, items, Collections.emptyList())
-                	  ul.setAuthor(gl.author)
-                	  ul.setCategory(gl.category)
-                	  ul.setInstitute(gl.institution)
-                	  ul.setDateCreated(gl.creationDate)
-                	  
-                	  if (gl.subtype != null) {
-                	    ListSubType lst = ListSubType.valueOf(gl.subtype)
-                	    ul.setListSubType(lst)
-                	  }
-                	  
-                	  ul.setId(gl.id)
-                	  
-                	  if (gl.origin != null) {
-                	    ListOrigin lo = ListOrigin.valueOf(gl.origin)
-                	    ul.setListOrigin(lo)
-                	  }
-                	  
-                	  userLists.add(ul)                	                   	  
-                   }
-                                                         
-//                   for (UserList ulist: userLists) {
-//                     userListBean.addList(ulist);
-//                   }	
-                   
-                   //orig way
-//                   UserListBean userListBean = new UserListBean();
-		           presentationCacheManager.addNonPersistableToSessionCache(request.getSession().getId(), 
-		        		   CacheConstants.USER_LISTS, userListBean);
-		            
-                   UserListBeanHelper userListBeanHelper = new UserListBeanHelper(request.getSession().getId());
-                   userListBeanHelper.addBean(request.getSession().getId(),CacheConstants.USER_LISTS,userListBean);            
-//                   List<UserList> userLists = (List<UserList>) listLoader.loadUserLists();
-                   for(UserList legacyul: userLists){
-                       userListBeanHelper.addList(legacyul);
-                   }
-                   //end orig
-                   
-                }                
                 System.out.println("Successfully stored UserListBean in the session")
     		}
     		else {
@@ -263,11 +184,15 @@ class ApplicationUserController extends BaseController {
    */
 
    def logout = {
-	session.userId = null
-	session.invalidate();
-    //need to clean the session entirely, lists, cache, temp files, ect....this is not enough
-	flash['message'] = 'Successfully logged out'
-	//redirect(controller:'applicationUser', action:'login')
-     redirect(uri: "/");
+		session.userId = null
+		//session.invalidate(); //dont invalidate, but rather start a new one - more to do here
+	    //need to clean the session entirely, lists, cache, temp files, ect....this is not enough
+	
+	    flash['message'] = 'Successfully logged out'
+		
+		//reload the default lists
+		defaultListLoaderService.loadDefaultLists()
+		
+	    redirect(uri: "/");
    }
 }
