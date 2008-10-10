120b636198443a0173d25838c8c5ae24eed46fa0
Harris Michael <harrismic>
2008-10-10 20:59:59 +0000

    Class for Rembrandt clinical reports.
    SVN-Revision: 5026

---
+++ src/java/gov/nih/nci/cma/clinical/RembrandtClinicalReportBean.java
@@ -0,0 +1,253 @@
+package gov.nih.nci.cma.clinical;
+
+public class RembrandtClinicalReportBean {
+	private String sampleId;
+	private String ageAtDx;	 
+	private String gender;
+	private String survivalMonths;	 
+	private String disease;	 
+	private String grade;	 
+	private String race;	 
+	private String institution;	 
+	private String karnofsky;	 
+	private String neurologicalExamOutcome;	 
+	private String mriDesc;	 
+	private String followupMonth;	 
+	private String steroidDoseStatus;	 
+	private String antiConvulsantStatus;	 
+	private String priorTherapyRadiationSite;	 
+	private String priorTherapyRadiationFractionDose;	 
+	private String priorTherapyRadiationFractionNumber;	 
+	private String priorTherapyRadiationType;	 
+	private String priorTherapyChemoAgentName;	 
+	private String priorTherapyChemoCourseCount;	 
+	private String priorTherapySurgeryProcedureTitle;	 
+	private String priorTherapySurgeryTumorHistology;	 
+	private String priorTherapySurgeryOutcome;	 
+	private String onStudyTherapyRadiationSite;	 
+	private String onStudyTherapyRadiationNeurosisStatus;	 
+	private String onStudyTherapyRadiationFractionDose;
+	private String onStudyTherapyRadiationFractionNumber;	 
+	private String onStudyTherapyRadiationType;	 
+	private String onStudyTherapyChemoAgentName;	 
+	private String onStudyTherapyChemoCourseCount;	 
+	private String onStudyTherapySurgeryProcedureTitle;	 
+	private String onStudyTherapySurgeryIndication;	 
+	private String onStudyTherapySurgeryHistoDiagnosis;	 
+	private String onStudyTherapySurgeryOutcome;
+	public String getSampleId() {
+		return sampleId;
+	}
+	public void setSampleId(String sampleId) {
+		this.sampleId = sampleId;
+	}
+	public String getAgeAtDx() {
+		return ageAtDx;
+	}
+	public void setAgeAtDx(String ageAtDx) {
+		this.ageAtDx = ageAtDx;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getSurvivalMonths() {
+		return survivalMonths;
+	}
+	public void setSurvivalMonths(String survivalMonths) {
+		this.survivalMonths = survivalMonths;
+	}
+	public String getDisease() {
+		return disease;
+	}
+	public void setDisease(String disease) {
+		this.disease = disease;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	public String getRace() {
+		return race;
+	}
+	public void setRace(String race) {
+		this.race = race;
+	}
+	public String getInstitution() {
+		return institution;
+	}
+	public void setInstitution(String institution) {
+		this.institution = institution;
+	}
+	public String getKarnofsky() {
+		return karnofsky;
+	}
+	public void setKarnofsky(String karnofsky) {
+		this.karnofsky = karnofsky;
+	}
+	public String getNeurologicalExamOutcome() {
+		return neurologicalExamOutcome;
+	}
+	public void setNeurologicalExamOutcome(String neurologicalExamOutcome) {
+		this.neurologicalExamOutcome = neurologicalExamOutcome;
+	}
+	public String getMriDesc() {
+		return mriDesc;
+	}
+	public void setMriDesc(String mriDesc) {
+		this.mriDesc = mriDesc;
+	}
+	public String getFollowupMonth() {
+		return followupMonth;
+	}
+	public void setFollowupMonth(String followupMonth) {
+		this.followupMonth = followupMonth;
+	}
+	public String getSteroidDoseStatus() {
+		return steroidDoseStatus;
+	}
+	public void setSteroidDoseStatus(String steroidDoseStatus) {
+		this.steroidDoseStatus = steroidDoseStatus;
+	}
+	public String getAntiConvulsantStatus() {
+		return antiConvulsantStatus;
+	}
+	public void setAntiConvulsantStatus(String antiConvulsantStatus) {
+		this.antiConvulsantStatus = antiConvulsantStatus;
+	}
+	public String getPriorTherapyRadiationSite() {
+		return priorTherapyRadiationSite;
+	}
+	public void setPriorTherapyRadiationSite(String priorTherapyRadiationSite) {
+		this.priorTherapyRadiationSite = priorTherapyRadiationSite;
+	}
+	public String getPriorTherapyRadiationFractionDose() {
+		return priorTherapyRadiationFractionDose;
+	}
+	public void setPriorTherapyRadiationFractionDose(
+			String priorTherapyRadiationFractionDose) {
+		this.priorTherapyRadiationFractionDose = priorTherapyRadiationFractionDose;
+	}
+	public String getPriorTherapyRadiationFractionNumber() {
+		return priorTherapyRadiationFractionNumber;
+	}
+	public void setPriorTherapyRadiationFractionNumber(
+			String priorTherapyRadiationFractionNumber) {
+		this.priorTherapyRadiationFractionNumber = priorTherapyRadiationFractionNumber;
+	}
+	public String getPriorTherapyRadiationType() {
+		return priorTherapyRadiationType;
+	}
+	public void setPriorTherapyRadiationType(String priorTherapyRadiationType) {
+		this.priorTherapyRadiationType = priorTherapyRadiationType;
+	}
+	public String getPriorTherapyChemoAgentName() {
+		return priorTherapyChemoAgentName;
+	}
+	public void setPriorTherapyChemoAgentName(String priorTherapyChemoAgentName) {
+		this.priorTherapyChemoAgentName = priorTherapyChemoAgentName;
+	}
+	public String getPriorTherapyChemoCourseCount() {
+		return priorTherapyChemoCourseCount;
+	}
+	public void setPriorTherapyChemoCourseCount(String priorTherapyChemoCourseCount) {
+		this.priorTherapyChemoCourseCount = priorTherapyChemoCourseCount;
+	}
+	public String getPriorTherapySurgeryProcedureTitle() {
+		return priorTherapySurgeryProcedureTitle;
+	}
+	public void setPriorTherapySurgeryProcedureTitle(
+			String priorTherapySurgeryProcedureTitle) {
+		this.priorTherapySurgeryProcedureTitle = priorTherapySurgeryProcedureTitle;
+	}
+	public String getPriorTherapySurgeryTumorHistology() {
+		return priorTherapySurgeryTumorHistology;
+	}
+	public void setPriorTherapySurgeryTumorHistology(
+			String priorTherapySurgeryTumorHistology) {
+		this.priorTherapySurgeryTumorHistology = priorTherapySurgeryTumorHistology;
+	}
+	public String getPriorTherapySurgeryOutcome() {
+		return priorTherapySurgeryOutcome;
+	}
+	public void setPriorTherapySurgeryOutcome(String priorTherapySurgeryOutcome) {
+		this.priorTherapySurgeryOutcome = priorTherapySurgeryOutcome;
+	}
+	public String getOnStudyTherapyRadiationSite() {
+		return onStudyTherapyRadiationSite;
+	}
+	public void setOnStudyTherapyRadiationSite(String onStudyTherapyRadiationSite) {
+		this.onStudyTherapyRadiationSite = onStudyTherapyRadiationSite;
+	}
+	public String getOnStudyTherapyRadiationNeurosisStatus() {
+		return onStudyTherapyRadiationNeurosisStatus;
+	}
+	public void setOnStudyTherapyRadiationNeurosisStatus(
+			String onStudyTherapyRadiationNeurosisStatus) {
+		this.onStudyTherapyRadiationNeurosisStatus = onStudyTherapyRadiationNeurosisStatus;
+	}
+	public String getOnStudyTherapyRadiationFractionDose() {
+		return onStudyTherapyRadiationFractionDose;
+	}
+	public void setOnStudyTherapyRadiationFractionDose(
+			String onStudyTherapyRadiationFractionDose) {
+		this.onStudyTherapyRadiationFractionDose = onStudyTherapyRadiationFractionDose;
+	}
+	public String getOnStudyTherapyRadiationFractionNumber() {
+		return onStudyTherapyRadiationFractionNumber;
+	}
+	public void setOnStudyTherapyRadiationFractionNumber(
+			String onStudyTherapyRadiationFractionNumber) {
+		this.onStudyTherapyRadiationFractionNumber = onStudyTherapyRadiationFractionNumber;
+	}
+	public String getOnStudyTherapyRadiationType() {
+		return onStudyTherapyRadiationType;
+	}
+	public void setOnStudyTherapyRadiationType(String onStudyTherapyRadiationType) {
+		this.onStudyTherapyRadiationType = onStudyTherapyRadiationType;
+	}
+	public String getOnStudyTherapyChemoAgentName() {
+		return onStudyTherapyChemoAgentName;
+	}
+	public void setOnStudyTherapyChemoAgentName(String onStudyTherapyChemoAgentName) {
+		this.onStudyTherapyChemoAgentName = onStudyTherapyChemoAgentName;
+	}
+	public String getOnStudyTherapyChemoCourseCount() {
+		return onStudyTherapyChemoCourseCount;
+	}
+	public void setOnStudyTherapyChemoCourseCount(
+			String onStudyTherapyChemoCourseCount) {
+		this.onStudyTherapyChemoCourseCount = onStudyTherapyChemoCourseCount;
+	}
+	public String getOnStudyTherapySurgeryProcedureTitle() {
+		return onStudyTherapySurgeryProcedureTitle;
+	}
+	public void setOnStudyTherapySurgeryProcedureTitle(
+			String onStudyTherapySurgeryProcedureTitle) {
+		this.onStudyTherapySurgeryProcedureTitle = onStudyTherapySurgeryProcedureTitle;
+	}
+	public String getOnStudyTherapySurgeryIndication() {
+		return onStudyTherapySurgeryIndication;
+	}
+	public void setOnStudyTherapySurgeryIndication(
+			String onStudyTherapySurgeryIndication) {
+		this.onStudyTherapySurgeryIndication = onStudyTherapySurgeryIndication;
+	}
+	public String getOnStudyTherapySurgeryHistoDiagnosis() {
+		return onStudyTherapySurgeryHistoDiagnosis;
+	}
+	public void setOnStudyTherapySurgeryHistoDiagnosis(
+			String onStudyTherapySurgeryHistoDiagnosis) {
+		this.onStudyTherapySurgeryHistoDiagnosis = onStudyTherapySurgeryHistoDiagnosis;
+	}
+	public String getOnStudyTherapySurgeryOutcome() {
+		return onStudyTherapySurgeryOutcome;
+	}
+	public void setOnStudyTherapySurgeryOutcome(String onStudyTherapySurgeryOutcome) {
+		this.onStudyTherapySurgeryOutcome = onStudyTherapySurgeryOutcome;
+	}	
+}
