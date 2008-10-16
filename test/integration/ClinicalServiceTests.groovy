c965c543537dfbc2e05a9e17678bfdab776499c8
Harris Michael <harrismic>
2008-10-16 16:30:00 +0000

    Clinical test code
    SVN-Revision: 5047

--- test/integration/ClinicalServiceTests.groovy
+++ test/integration/ClinicalServiceTests.groovy
@@ -9,14 +9,26 @@ class ClinicalServiceTests extends GroovyTestCase {
     	
     	System.out.println(cols)
     	List ids = new ArrayList()
-    	ids.add("E007")
-    	ids.add("E009")
+    	ids.add("E09910")
+    	ids.add("HF1292")
+    	ids.add("E09928")
+    	ids.add("E07733")
+    	ids.add("E09358")
         List data = clinicalService.getClinicalData(ids)
+        System.out.println("Got back numResults=${data?.size()}")
+        
     	data.each { d -> 
     	        System.out.print(d)
     	        System.out.print("\t")
     	}
     	System.out.println()
+    	
+    	
+    	List diseaseValues = clinicalService.getPermissibleValues("disease")
+    	System.out.println("Got disease values:")
+    	diseaseValues.each { d -> System.out.println("${d},") }
+    	System.out.println()
+    	
     	assert true
     }
 }
