fdacca1b6b7fd4ff070e6bf8e6132e56f76a18b3
Harris Michael <harrismic>
2008-10-16 16:27:37 +0000

    Refactored.
    SVN-Revision: 5043

--- src/java/gov/nih/nci/cma/util/ClinParmsComparator.java
+++ src/java/gov/nih/nci/cma/util/ClinParmsComparator.java
@@ -1,16 +1,16 @@
 package gov.nih.nci.cma.util;
 
 import java.util.Comparator;
-import gov.nih.nci.cma.domain.CmaRembClin2;
+import gov.nih.nci.cma.domain.CmaRembClin;
 
 public class ClinParmsComparator implements Comparator {
 
 	public int compare(Object o1, Object o2) {
-		if (!(o1 instanceof CmaRembClin2) || !(o2 instanceof CmaRembClin2)) {
+		if (!(o1 instanceof CmaRembClin) || !(o2 instanceof CmaRembClin)) {
 		  return 0;
 		}
-		CmaRembClin2 d1 = (CmaRembClin2) o1;
-		CmaRembClin2 d2 = (CmaRembClin2) o2;
+		CmaRembClin d1 = (CmaRembClin) o1;
+		CmaRembClin d2 = (CmaRembClin) o2;
 		String p1 = d1.getParm();
 		String p2 = d2.getParm();
 		return p1.compareTo(p2);
