a4febe2859b8db9c923e08c44a076965feeed97a
Harris Michael <harrismic>
2008-10-20 15:00:28 +0000

    More tests for the clinical service.
    SVN-Revision: 5075

--- test/integration/ClinicalServiceTests.groovy
+++ test/integration/ClinicalServiceTests.groovy
@@ -1,3 +1,5 @@
+
+
 class ClinicalServiceTests extends GroovyTestCase {
 
 	def clinicalService
@@ -8,12 +10,44 @@ class ClinicalServiceTests extends GroovyTestCase {
     	def cols = clinicalService.getColumnNames()
     	
     	System.out.println(cols)
-    	List ids = new ArrayList()
-    	ids.add("E09910")
-    	ids.add("HF1292")
-    	ids.add("E09928")
-    	ids.add("E07733")
-    	ids.add("E09358")
+//    	List ids = new ArrayList()
+//    	ids.add("E09910")
+//    	ids.add("HF1292")
+//    	ids.add("E09928")
+//    	ids.add("E07733")
+//    	ids.add("E09358")
+    	
+    	//List ids = clinicalService.getIdsForAgeAtDx(new Integer(40), new Integer(60))
+    	//List ids = clinicalService.getIdsForGender("F")
+    	
+//    	String[] patientGroups = new String[2]
+//    	patientGroups[0] = "GBM"
+//    	patientGroups[1] = "NON_TUMOR"
+
+		List<String> patientGroups = new ArrayList<String>()
+    	patientGroups.add("GBM")
+    	patientGroups.add("NON_TUMOR")
+    	
+    	List ids = clinicalService.getIdsForSampleGroups(patientGroups)
+    	
+    	System.out.println("getIdsForSampleGroups returned ids:")
+    	ids.each { id ->
+    		System.out.println("${id}")
+    		
+    	}
+    	
+        List data = clinicalService.getClinicalData(ids)
+        System.out.println("Got back numResults=${data?.size()}")
+        System.out.println("==== CLINICAL REPORT ====")
+    	data.each { d -> 
+    	        System.out.println(d)    	        
+    	}
+    	System.out.println("===== END CLINICAL REPORT ====")
+    	System.out.println()
+    	
+    	
+    	/*
+    	
         List data = clinicalService.getClinicalData(ids)
         System.out.println("Got back numResults=${data?.size()}")
         System.out.println("==== CLINICAL REPORT ====")
@@ -27,6 +61,10 @@ class ClinicalServiceTests extends GroovyTestCase {
     	System.out.println("Got disease values:")
     	diseaseValues.each { d -> System.out.println("${d},") }
     	System.out.println()
+    	*/
+    	
+    	
+    	
     	
     	assert true
     }
