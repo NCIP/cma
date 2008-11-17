36994a75a93fb4888c31d200bfe979d7129c65ff
Mark Lewis <lewisma2>
2008-11-17 22:54:17 +0000

    Added comments explaining the login bypass.
    SVN-Revision: 5198

--- grails-app/conf/CMAFilters.groovy
+++ grails-app/conf/CMAFilters.groovy
@@ -2,34 +2,37 @@ class CMAFilters {
    def filters = {
         manageLicenseAcceptance(controller:'*') {
 			before = {
-			if(controllerName != "applicationUser")	{
 				//println(" *** CMAFilter")
 				//println("controllerName = " + controllerName)
 				//println("actionName = " + actionName)
-
-				// Try session
-				def acceptedLicense = session.acceptedLicense
-				//println("session: acceptedLicense = " + acceptedLicense)
-				// Try params (set in legal.gsp)
-				if (acceptedLicense == null) {
-					acceptedLicense = params.acceptedLicense
-					//println("params: acceptedLicense = " + acceptedLicense)
+				
+				// 'applicationUser' means the user is logging in. Bypass
+				// the license check for this controller.
+				if (controllerName != "applicationUser") {
+					// Try session
+					def acceptedLicense = session.acceptedLicense
+					//println("session: acceptedLicense = " + acceptedLicense)
+					// Try params (set in legal.gsp)
+					if (acceptedLicense == null) {
+						acceptedLicense = params.acceptedLicense
+						//println("params: acceptedLicense = " + acceptedLicense)
+					}
+					if (acceptedLicense == null) {
+						// Go to license. Have to set the user's controller choice
+						// in session; setting in request or params doesn't stick
+						session.uController = controllerName					
+						redirect(uri:'/legal.gsp')
+		                return false
+					}
+					else {
+						session.acceptedLicense = "true"
+						session.uController = null
+					}
 				}
-				if (acceptedLicense == null) {
-					// Go to license. Have to set the user's controller choice
-					// in session; setting in request or params doesn't stick
-					session.uController = controllerName
-					redirect(uri:'/legal.gsp')
-					
-	                return false
-				}
-				else {
-					session.acceptedLicense = "true"
-					session.uController = null
-				}			
-			}
+				// else allow to proceed 
 			}
 		}
+        
    }
 }
 
