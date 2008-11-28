b6a73adaf50e3af7a5bda6088c4743c49b0bd197
no_author <no_author@no_author.cvs2svn>
2008-11-28 14:37:08 +0000

    This commit was manufactured by cvs2svn to create tag 'CMA_28NOV2008_B2'.
    SVN-Revision: 5235

---
+++ CMA_28NOV2008_B2/src/java/gov/nih/nci/cma/util/RbtNeuroComparator.java
@@ -0,0 +1,22 @@
+package gov.nih.nci.cma.util;
+
+import java.util.Comparator;
+import gov.nih.nci.cma.domain.rembrandt.NeurologicalEvaluation;
+
+public class RbtNeuroComparator implements Comparator {
+
+	public int compare(Object o1, Object o2) {
+		NeurologicalEvaluation n1 = (NeurologicalEvaluation) o1;
+		NeurologicalEvaluation n2 = (NeurologicalEvaluation) o2;
+		
+		java.math.BigDecimal f1 = n1.getFollowupMonth();
+		java.math.BigDecimal f2 = n2.getFollowupMonth();
+		
+		if ((n1.getFollowupMonth() != null) &&
+			(n2.getFollowupMonth() != null)) {		
+			return f1.compareTo(f2);
+		}
+		return 0;
+	}
+
+}
