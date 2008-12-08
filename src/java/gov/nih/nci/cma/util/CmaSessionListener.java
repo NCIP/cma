4e7d3d8b27dd694aea0788eed6460468728698b0
Harris Michael <harrismic>
2008-12-08 22:40:13 +0000

    Added code to do cleanup.
    SVN-Revision: 5252

--- src/java/gov/nih/nci/cma/util/CmaSessionListener.java
+++ src/java/gov/nih/nci/cma/util/CmaSessionListener.java
@@ -2,6 +2,13 @@ package gov.nih.nci.cma.util;
 
 
 
+import gov.nih.nci.caintegrator.application.cache.BusinessCacheManager;
+import gov.nih.nci.caintegrator.application.cache.PresentationCacheManager;
+import gov.nih.nci.caintegrator.security.PublicUserPool;
+import gov.nih.nci.caintegrator.service.task.Task;
+
+import java.util.Collection;
+
 import org.apache.commons.logging.Log; 
 import org.apache.commons.logging.LogFactory;
 
@@ -16,7 +23,21 @@ public class CmaSessionListener implements HttpSessionListener {
 	}
 	
 	public void sessionDestroyed(HttpSessionEvent event) { 
-		System.out.println("Session destroyed id=" + event.getSession().getId() ); 
+		System.out.println("Session destroyed cleaning cache for id=" + event.getSession().getId() );
+				
+        BusinessCacheManager.getInstance().removeSessionCache(event.getSession().getId());
+		
+        Collection<Task> allTasks = PresentationCacheManager.getInstance().getAllSessionTasks(event.getSession().getId());
+        BusinessCacheManager.getInstance().removeSessionCacheForTasks(allTasks);
+        
+        PresentationCacheManager.getInstance().removeSessionCache(event.getSession().getId());
+        
+        String gpUser = (String)event.getSession().getAttribute(PublicUserPool.PUBLIC_USER_NAME);
+    	PublicUserPool pool = (PublicUserPool)event.getSession().getAttribute(PublicUserPool.PUBLIC_USER_POOL);
+    	
+    	if (gpUser != null && pool != null){
+			pool.returnPublicUser(gpUser);
+    	}               
 	} 
 } 
 
