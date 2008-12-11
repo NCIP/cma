26842873ccdf004bc6072b4f334938925bd77a0b
Harris Michael <harrismic>
2008-12-11 21:03:47 +0000

    TARGET Gorm
    SVN-Revision: 5269

---
+++ grails-app/domain/gov/nih/nci/cma/domain/target/TargetClinicalStg.groovy
@@ -0,0 +1,30 @@
+package gov.nih.nci.cma.domain.target
+
+class TargetClinicalStg {
+	  private String ptId;                  
+	  private String targetID;              
+	  private String gender;    
+	  private String naaccrRace; 
+	  private String naaccrEthnicity;
+	  private String congenitalAbnormality; 
+	  private String age; 
+	  private String pbWbc;
+	  private String cns; 
+	  private String testicular; 
+	  private String karyotype; 
+	  private String mrdDay8;
+	  private String mrdDay29; 
+	  private String bmaBlastsDay8; 
+	  private String bmaBlastsDay29; 
+	  private String event;
+	  private String timeToEvent;
+	  private String death; 
+	  private String timeToDeath; 
+	  private String siteOfRelapse; 
+	  private String telStatus; 
+	  private String trisomies_4_10;
+	  private String mllStatus; 
+	  private String e2aStatus; 
+	  private String bcrStatus; 
+	  private String dnaIndex;
+}
