7947fd7b0716471dc7d4f0a0f6ee9e0b8073b37b
Harris Michael <harrismic>
2008-12-11 19:47:10 +0000

    TARGET clinical service.
    SVN-Revision: 5260

--- grails-app/services/TARGETClinicalService.groovy
+++ grails-app/services/TARGETClinicalService.groovy
@@ -1,4 +1,29 @@
+import java.util.Collections;
+
 
 class TARGETClinicalService {
 	
+	/**
+	 * Get permissible values for a given parameter
+	 */
+	public List<String> getPermissibleValues(String paramName) {
+		
+		return Collections.emptyList();
+	}
+	
+	/**
+	 * Get the clinical data given a clinical form.
+	 */
+	def getClinicalData = { clinicalForm -> 
+	
+	  return Collections.emptyList();
+	}
+	
+	 /**
+	  * Get the clinical data for a sample group
+	  */
+	 public List getClinicalDataForGroup(String groupName) {
+		 
+		 return Collections.emptyList();
+	 }
 }
