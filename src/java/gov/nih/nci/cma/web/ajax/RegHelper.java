0f30d4648901afc3f33757467d2ccaa907b789b5
Ryan Landy <landyr>
2009-02-25 23:38:14 +0000

    merged from branch - dont read from system props
    SVN-Revision: 5317

--- src/java/gov/nih/nci/cma/web/ajax/RegHelper.java
+++ src/java/gov/nih/nci/cma/web/ajax/RegHelper.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.json.simple.JSONObject;
+import java.util.Map;
 
 import org.directwebremoting.WebContext;
 import org.directwebremoting.WebContextFactory;
@@ -11,6 +12,8 @@ import org.directwebremoting.WebContextFactory;
 import gov.nih.nci.caintegrator.application.mail.Mail;
 import gov.nih.nci.caintegrator.application.mail.MailProps;
 import gov.nih.nci.caintegrator.exceptions.ValidationException;
+
+import org.codehaus.groovy.grails.commons.ConfigurationHolder;
 
 public class RegHelper {
 	
@@ -41,7 +44,9 @@ public class RegHelper {
 	        	fdbk += "Their institution is: " + in + " \n";
 	        	fdbk += "\n\nThis is an automated email sent from the CMA Application.\n";
 	        	*/
-	        	String fdbk = System.getProperty("cma.register.template.support");
+	        	//String fdbk = System.getProperty("cma.register.template.support");
+	        	Map propsMap = (Map) ConfigurationHolder.getFlatConfig();
+	        	String fdbk = (String) propsMap.get("cma.register.template.support"); 
 	        	fdbk = fdbk.replace("{last_name}", (ln!=null)? ln : "None");
 	        	fdbk = fdbk.replace("{first_name}", (fn!=null)? fn : "None");
 	        	fdbk = fdbk.replace("{email}", (em!=null)? em : "None");
@@ -51,16 +56,16 @@ public class RegHelper {
 	        	
 	        	mp.setBody(fdbk);
 	        	//the fields below should be in a props file
-	         	mp.setSmtp(System.getProperty("cma.feedback.mailSMPT"));
-	        	mp.setSubject(System.getProperty("cma.register.mailSubject.support"));
-	        	mp.setMailTo(System.getProperty("cma.register.mailTo.support"));
-	        	mp.setMailFrom(System.getProperty("cma.feedback.mailFrom"));
+	         	mp.setSmtp((String) propsMap.get("cma.feedback.mailSMPT"));
+	        	mp.setSubject((String) propsMap.get("cma.register.mailSubject.support"));
+	        	mp.setMailTo((String) propsMap.get("cma.register.mailTo.support"));
+	        	mp.setMailFrom((String) propsMap.get("cma.feedback.mailFrom"));
 
 	    		Mail.sendMail(mp);
 	    		
 	    		//send the mail to the user
 	    		mp = new MailProps();
-	    		fdbk = System.getProperty("cma.register.template.user");
+	    		fdbk = (String) propsMap.get("cma.register.template.user");
 	        	fdbk = fdbk.replace("{last_name}", (ln!=null)? ln : "None");
 	        	fdbk = fdbk.replace("{first_name}", (fn!=null)? fn : "None");
 	    		/*
@@ -68,10 +73,10 @@ public class RegHelper {
 	    		fdbk += "\n\nSincerely,\n-The CMA Team";
 	    		*/
 	    		mp.setBody(fdbk);
-	    		mp.setSmtp(System.getProperty("cma.feedback.mailSMPT"));
-	        	mp.setSubject(System.getProperty("cma.register.mailSubject.user"));
+	    		mp.setSmtp((String) propsMap.get("cma.feedback.mailSMPT"));
+	        	mp.setSubject((String) propsMap.get("cma.register.mailSubject.user"));
 	        	mp.setMailTo(em.trim());
-	        	mp.setMailFrom(System.getProperty("cma.feedback.mailFrom"));
+	        	mp.setMailFrom((String) propsMap.get("cma.feedback.mailFrom"));
 
 	        	Mail.sendMail(mp);
 	    	}
