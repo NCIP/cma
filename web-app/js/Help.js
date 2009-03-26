461856410bf0026e991a030e122fcb1957b8c2ef
Ryan Landy <landyr>
2009-03-26 14:27:24 +0000

    new help utility
    SVN-Revision: 5350

--- web-app/js/Help.js
+++ web-app/js/Help.js
@@ -1,65 +1,31 @@
 var Help = {
-	url : "helpDocs/TCGA_Analysis_Online_Help/index.html?single=false&context=TCGA_Analysis_Online_Help&topic=",
-	popHelp: function(topic) {
-		//alert("coming soon");
+	url : "helpDocs/cmaPortal/index.html?single=false&context=cmaPortal&topic=",
+	popGuide: function()	{
 		window.open("helpDocs/cmap_user_guide.pdf", "Help", "status,scrollbars,resizable,width=800,height=500");
-		
-		//window.open (Help.url+topic, "Help", "status,scrollbars,resizable,width=800,height=500");  
+	},
+	popHelp: function(topic) {
+		window.open (Help.url+topic, "Help", "status,scrollbars,resizable,width=800,height=500");  
 		//use the below if you want the "always on top" feature, most dont like it
 		//window.open (Help.url+topic, "Help", "alwaysRaised,dependent,status,scrollbars,resizable,width=800,height=500");  
-		//alert(Help.url+topic);
 	},
-	insertHelp: function(topic)	{
-		var ex = arguments[1] ? arguments[1] : "";
-		var exst = arguments[2] ? arguments[2] : "";
-		var pth = arguments[3] ? arguments[3] : "";
-		var htm = "<img "+ ex + " style=\"cursor:pointer;border:0px;"+ exst + "\" src=\""+pth+"images/help.png\" alt=\"help\" id=\"helpIcon\" name=\"helpIcon\" onclick=\"Help.popHelp(\'"+topic+"\');\" />";
-		document.write(htm);
+	insertHelp: function(opts)	{
+		var ex = opts.ex ? opts.ex : "";
+		var exst = opts.exst ? opts.exst : "";
+		var pth = opts.pth ? opts.pth : "images";
+		var topic = opts.topic ? opts.topic.replace('helptag_','') : "welcome";
+		var htm = "<img "+ ex + " style=\"cursor:pointer;border:0px;"+ exst + "\" src=\""+pth+"/help.gif\" alt=\"help\" id=\"helpIcon\" name=\"helpIcon\" onclick=\"Help.popHelp(\'"+topic+"\');\" />";
+		return htm;
+	},
+	parsePage: function(opts)	{
+		$$('div.help').each( function(e)	{
+			e.update(Help.insertHelp({pth: opts.pth,topic:e.id,exst:'float:right;'}));
+		});
 	},
 	popHelpMain: function(topic) {
-		var _url = "helpDocs/TCGA_Analysis_Online_Help/Welcome.1.1.html";
-		//alert("coming soon");
-		window.open("helpDocs/cmap_user_guide.pdf", "Help", "status,scrollbars,resizable,width=800,height=500");
-		
-		//window.open (_url+topic, "Help", "status,scrollbars,resizable,width=800,height=500");  
+		var _url = "helpDocs/cmaPortal/Welcome.1.1.html";
+		window.open (_url+topic, "Help", "status,scrollbars,resizable,width=800,height=500");  
 		//use the below if you want the "always on top" feature, most dont like it		
 		//window.open (_url+topic, "Help", "alwaysRaised,dependent,status,scrollbars,resizable,width=800,height=500");  
-		//alert(Help.url+topic);
-	},
-	convertHelp: function(topic)	{
-		var ex = arguments[1] ? arguments[1] : "";
-		var exst = arguments[2] ? arguments[2] : "";
-		var ta = "";
-		switch(topic)	{
-			case "clinicalquery":
-				ta = "Clinical_query_analysis";
-				break;
-			case "correlationscatter":
-				ta = "Correlation_scatter_analysis";
-				break;				
-			case "categoricalcorrelation":
-				ta = "Explanatory_data_analysis";
-				break;
-			case "ihclevelquery":
-				ta = "IHC_LOE_query_analysis";
-				break;
-			case "ihclossquery":
-				ta = "IHC_Loss_query_analysis";
-				break;
-			case "classcomparison":
-				ta = "Class_comparison";
-				break;
-			case "principalcomponent":
-				ta = "PCA_analysis";
-				break;
-			case "hierarchicalclustering":
-				ta = "HCA_analysis";
-				break;			
-			default:
-				break;
-		
-		}
-		Help.insertHelp(ta, ex, exst);
-	
 	}
-}
+	
+};
