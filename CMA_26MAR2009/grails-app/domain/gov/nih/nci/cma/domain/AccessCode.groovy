461a71fc4d5c357875db0a56c80755585d7982a3
Ryan Landy <landyr>
2009-03-26 14:35:37 +0000

    1.1 RC1
    SVN-Revision: 5351

---
+++ CMA_26MAR2009/grails-app/domain/gov/nih/nci/cma/domain/AccessCode.groovy
@@ -0,0 +1,11 @@
+package gov.nih.nci.cma.domain
+
+class AccessCode {
+	/*
+	 *   ACCESS_CODE  VARCHAR2(20 BYTE),
+	 *   DESCRIPTION  VARCHAR2(50 BYTE),
+	 */
+	 String accessCode
+	 String description
+
+}
