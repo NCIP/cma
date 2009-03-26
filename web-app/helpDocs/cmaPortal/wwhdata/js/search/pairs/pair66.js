9e213baf04e0b86b42d224701f9749a1649f3d2d
Ryan Landy <landyr>
2009-03-26 14:41:59 +0000

    initial help checkin
    SVN-Revision: 5353

---
+++ web-app/helpDocs/cmaPortal/wwhdata/js/search/pairs/pair66.js
@@ -0,0 +1,22 @@
+function FileData_Pairs(x)
+{
+x.t("heatmap","viewer");
+x.t("working","genome");
+x.t("cma","portal");
+x.t("access","cma");
+x.t("details","working");
+x.t("direct","access");
+x.t("provides","direct");
+x.t("portal","genome");
+x.t("viewer","heatmap");
+x.t("viewer","tool");
+x.t("viewer","analysis");
+x.t("viewer","integrated");
+x.t("tools","integrated");
+x.t("view","details");
+x.t("tool","provides");
+x.t("genome","view");
+x.t("genome","views");
+x.t("analysis","tools");
+x.t("integrated","heatmap");
+}
