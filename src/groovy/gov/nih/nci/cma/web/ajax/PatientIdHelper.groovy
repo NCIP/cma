9110353292d78d7b638f6c4ec9c59f50f610500d
Johnita Beasley <beasleyj@mail.nih.gov>
2009-11-05 15:24:40 +0000

    Added to support PatientId Mappings
    SVN-Revision: 5534

---
+++ src/groovy/gov/nih/nci/cma/web/ajax/PatientIdHelper.groovy
@@ -0,0 +1,49 @@
+package gov.nih.nci.cma.web.ajax;
+
+  import java.util.List
+
+  import org.codehaus.groovy.grails.commons.ConfigurationHolder;
+  
+  import gov.nih.nci.cma.domain.IdMapping
+
+class PatientIdHelper {
+    
+    public List translateIds(List ids) {
+    
+		System.out.println("\n******************************************************************\n");
+		System.out.println("\nEntering PatientIdHelper::translateId....");
+    
+		Map propsMap = (Map) ConfigurationHolder.getFlatConfig();
+    	String dataContext = (String) propsMap.get("cma.dataContext"); 
+		
+		System.out.println("\nPatientIdHelper::translateId  The Data Context is => " + dataContext);
+		System.out.println("\nPatientIdHelper::translateId  Ids received => " + ids);
+     	
+		List<String> translatedIds = new ArrayList<String>();
+		
+		if ( dataContext.equals("TCGA") || dataContext.equals("TCGAOvarian") ) {
+		
+	      	ids.each { id ->
+				System.out.println("\nLeaving TestHelper::translateId  Translated Id " + id + " to => " + id.substring(0,12));
+			    translatedIds.add(id.substring(0,12))  
+	        }
+		
+		} else if ( dataContext.equals("TARGET") ) {
+		
+			List idMappingObjs = gov.nih.nci.cma.domain.IdMapping.list()			
+	      	ids.each { id ->
+	      		idMappingObjs.each { idMapping ->
+			        if (idMapping.getAnalysisFileId() == id) {
+						System.out.println("\nLeaving TestHelper::translateId  Translated Id " + id + " to => " + idMapping.getPdid());
+			          	translatedIds.add(idMapping.getPdid())  
+			        }
+		        }
+	        }
+		}
+
+		System.out.println("\nLeaving PatientIdHelper::translateId....");
+		System.out.println("\n******************************************************************\n");
+				
+		return translatedIds;
+    }
+}
