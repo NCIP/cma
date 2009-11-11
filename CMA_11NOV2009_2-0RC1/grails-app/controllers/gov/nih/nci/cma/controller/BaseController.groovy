35ece039777f7f842e16629f5cde5a1fbe09de5e
Johnita Beasley <beasleyj@mail.nih.gov>
2009-11-11 16:38:51 +0000

    SVN-Revision: 5541

---
+++ CMA_11NOV2009_2-0RC1/grails-app/controllers/gov/nih/nci/cma/controller/BaseController.groovy
@@ -0,0 +1,13 @@
+package gov.nih.nci.cma.controller
+
+abstract class BaseController {
+	def auth() {
+		if(!session.userId) {
+                def originalRequestParams =[controller:controllerName,action:actionName]
+		     originalRequestParams.putAll(params)
+		     session.originalRequestParams = originalRequestParams
+		    redirect(controller:'applicationUser',action:'login')
+		    return false
+		}
+	}
+}
---
+++ CMA_11NOV2009_2-0RC1/test/integration/gov/nih/nci/cma/domain/TcgaPublicViewTests.groovy
@@ -0,0 +1,8 @@
+package gov.nih.nci.cma.domain
+
+class TcgaPublicViewTests extends GroovyTestCase {
+
+    void testSomething() {
+
+    }
+}
