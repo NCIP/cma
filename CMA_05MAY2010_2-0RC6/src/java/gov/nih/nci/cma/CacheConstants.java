902c7e1da9ea0cc8f42e4a6a6d9f6ada28160527
Johnita Beasley <beasleyj@mail.nih.gov>
2010-05-05 20:38:57 +0000

    SVN-Revision: 5576

---
+++ CMA_05MAY2010_2-0RC6/src/java/gov/nih/nci/cma/CacheConstants.java
@@ -0,0 +1,7 @@
+package gov.nih.nci.cma;
+
+public final class CacheConstants {
+	public static final String USER_LISTS = "userLists";
+	public static final String ALL_USER_LISTS = "All_Patients";
+	public static final String SURVIVAL = "survival";
+}
